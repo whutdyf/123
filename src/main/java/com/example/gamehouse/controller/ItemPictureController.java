14bd7b48124a47ae92d669941077a4d6dd811ac8
whutdyf <1052719537@qq.com>
2022-06-20 17:28:42 +0800

    修改

--- src/main/java/com/example/gamehouse/controller/ItemPictureController.java
+++ src/main/java/com/example/gamehouse/controller/ItemPictureController.java
@@ -5,10 +5,7 @@ import com.example.gamehouse.Entity.ItemPicture;
 import com.example.gamehouse.service.ItemPictureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Base64;
 
@@ -18,7 +15,14 @@ import java.util.Base64;
 public class ItemPictureController {
 
     @Autowired
-    ItemPictureService ItemPictureService;
+    ItemPictureService itemPictureService;
+
+    @PostMapping(value = "/addPicture")
+    public boolean getPicture(ItemPicture itemPicture){
+
+        itemPictureService.add(itemPicture);
+        return true;
+    }
 
 
 
@@ -26,7 +30,7 @@ public class ItemPictureController {
     @ResponseBody
     public byte[] getPicture(String itemName) throws Exception {
 
-        ItemPicture itemPicture = ItemPictureService.findPicture(itemName);
+        ItemPicture itemPicture = itemPictureService.findPicture(itemName);
         byte[] bytes = Base64.getDecoder().decode(itemPicture.getPicture());
         return bytes;
 
